6ffeb933e0649f92f9b079aa8abde4abfe7cdf4e
alextagun <alextagun@gmail.com>
2019-09-04 12:29:56 +0300

    functional for multithreading

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import telebot
+import collections
 from QuestionManager import QuestionManager
 from Answer import Answer
 from QuizQuestion import QuizQuestion
 from State import State
 
-# questionManager = QuestionManager()
-state = State.Start
+class User(object):
+	def __init__(self, uid, state):
+		self.id = uid
+		self.state = state
+		self.questionManager = QuestionManager()
+
+class DialogBot(object):
+
+    def __init__(self):
+    	self.users = collections.defaultdict(User)
+
+dialogBot = DialogBot()
+# state = State.Start
 
 bot = telebot.TeleBot('648232646:AAHfynYQnWd_xD1knO6gZ7bE4V2_hQI_EZE')
-keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
-keyboard.add(telebot.types.KeyboardButton(text="Викторина"))
-keyboard.add(telebot.types.KeyboardButton(text="Верю не верю"))
+
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard)
+	global dialogBot
+	dialogBot.users[message.chat.id] = User(message.chat.id, State.Start)
+	keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+	keyboard.add(telebot.types.KeyboardButton(text="Викторина"))
+	keyboard.add(telebot.types.KeyboardButton(text="Верю не верю"))
+	bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+	global dialogBot
+	state = dialogBot.users[message.chat.id].state
 	print(state)
 	if state == State.Start:
 	    if message.text.lower() == 'викторина':
@@ -25,18 +43,18 @@ def send_text(message):
 	    elif message.text.lower() == 'верю не верю':
 	        startTruthOrLieQuiz(message)
 	elif state == State.CountryQuiz:
-		if message.text == questionManager.currQuestion.answers[0].text:
+		if message.text == dialogBot.users[message.chat.id].questionManager.currQuestion.answers[0].text:
 			pickAnswer(message, 0)
-		elif message.text == questionManager.currQuestion.answers[1].text:
+		elif message.text == dialogBot.users[message.chat.id].questionManager.currQuestion.answers[1].text:
 			pickAnswer(message, 1)
-		elif message.text == questionManager.currQuestion.answers[2].text:
+		elif message.text == dialogBot.users[message.chat.id].questionManager.currQuestion.answers[2].text:
 			pickAnswer(message, 2)
-		elif message.text == questionManager.currQuestion.answers[3].text:
+		elif message.text == dialogBot.users[message.chat.id].questionManager.currQuestion.answers[3].text:
 			pickAnswer(message, 3)
 		elif message.text.lower() == 'далее':
-			print(questionManager.cnt, questionManager.maxQuestions)
+			print(dialogBot.users[message.chat.id].questionManager.cnt, dialogBot.users[message.chat.id].questionManager.maxQuestions)
 
-			if(questionManager.cnt < questionManager.maxQuestions):
+			if(dialogBot.users[message.chat.id].questionManager.cnt < dialogBot.users[message.chat.id].questionManager.maxQuestions):
 				continueCountryQuiz(message)
 			else:
 				showResults(message)
@@ -44,10 +62,10 @@ def send_text(message):
 
 
 def startCountryQuiz(message):
-	global state
-	state = State.CountryQuiz
-	global questionManager
-	questionManager = QuestionManager()
+	global dialogBot
+	dialogBot.users[message.chat.id].state = State.CountryQuiz
+	dialogBot.users[message.chat.id].questionManager = QuestionManager()
+	questionManager = dialogBot.users[message.chat.id].questionManager
 	question = questionManager.getQuestion()
 		
 	keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
@@ -59,6 +77,8 @@ def startCountryQuiz(message):
 	bot.send_message(message.chat.id, question.text, reply_markup=keyboard)
 
 def continueCountryQuiz(message):
+	global dialogBot
+	questionManager = dialogBot.users[message.chat.id].questionManager
 	question = questionManager.getQuestion()
 		
 	keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
@@ -70,6 +90,8 @@ def continueCountryQuiz(message):
 	bot.send_message(message.chat.id, question.text, reply_markup=keyboard)
 
 def pickAnswer(message, num):
+	global dialogBot
+	questionManager = dialogBot.users[message.chat.id].questionManager
 	keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
 	keyboard.add(telebot.types.KeyboardButton(text='Далее'))
 	if(questionManager.checkAnswer(num)):
@@ -91,8 +113,9 @@ def startTruthOrLieQuiz(message):
 	pass
 
 def showResults(message):
-	global state
-	state = State.Start
+	global dialogBot
+	dialogBot.users[message.chat.id].state = State.Start
+	questionManager = dialogBot.users[message.chat.id].questionManager
 	text = 'Ваш результат: ' + str(questionManager.rightAnswers) + '/' + str(questionManager.maxQuestions)
 
 	keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
